6c57a169e6529bb18b1f66dae8452fb707ee0aff
jcarrionramos <volker.ball.rt@gmail.com>
2022-10-17 12:31:35 -0300

    WIP

---
+++ express-server/app.js
@@ -0,0 +1,32 @@
+import express from "express";
+import * as handlebars from "express-handlebars"
+
+import userRouter from "./routes/user.js"
+import dashboardRouter from "./routes/dashboard.js"
+
+const app = express();
+
+app.set("view engine", 'hbs')
+app.engine('hbs', handlebars.engine({
+  extname: 'hbs',
+  layoutsDir: `${__dirname}/views/layouts`,
+  partialsDir: `${__dirname}/views/partials`,
+  defaultLayout: 'index'
+}))
+
+
+app.use(express.json());
+app.use(express.static('public'))
+
+app.use("/user", userRouter);
+app.use("/dashboard", dashboardRouter)
+
+app.get('/', (req, res) => {
+  res.send("<h1>Hello world</h1>");
+});
+
+app.get("/about", (req, res) => {
+  res.send("<h1>About</h1>");
+});
+
+export { app };
--- express-server/routes/dashboard.js
+++ express-server/routes/dashboard.js
@@ -2,7 +2,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("<h1>Dashboard GET</h1>")
+  res.render('main');
 })
 
 router.post("/", (req, res) => {
